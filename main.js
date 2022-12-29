f228e7a9c7c669e8f2e7b2dcb0a52ab39df3a417
Magelulw <magelulw@gmail.com>
2022-12-29 09:57:52 +0100

    added onclick Events for the features and added that you need to hold and move your mouse to draw

--- main.js
+++ main.js
@@ -1,35 +1,101 @@
 const paintingWall = document.querySelector(".paintingWall");
 const container = document.querySelector(".container");
 
-wallWidthSize = paintingWall.style.width = "40rem";
-wallHeightSize = paintingWall.style.height = "40rem";
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
-let gridSize = 8;
+//* all the buttons and features;
+const penColorChanger = document.querySelector("#colorPicker");
+const rainbowClr = document.querySelector("#rainbowMode");
+const eraserBtn = document.querySelector("#eraser");
+const clearBtn = document.querySelector("#clear");
+const sizeValue = document.querySelector("#sizeValue")
+const gridRange = document.querySelector("#gridSize")
+
+let gridSize = gridRange.value;
+let currentMode = "default";
+
+sizeValue.textContent = `${gridSize} x ${gridSize}`
 
 
-let wallWidth = wallWidthSize.replace("rem","");
-let wallHeight = wallHeightSize.replace("rem","");
+penColorChanger.oninput = () => {currentMode }
+rainbowClr.onclick = () => {currentMode = "rainbow"};
+eraserBtn.onclick = () => {currentMode = "eraser"};
+clearBtn.onclick = () => setGridToTheStart()
+
+
 
 paintingWall.style["grid-template-columns"] = `repeat(${gridSize},1fr)`;
 
+function runGame(){
+    createGrid()
+    setGridSize()
+}
+runGame()
 
-for(let row = 0; row < gridSize; row++){
-    for(let col = 0; col < gridSize; col++){
-        const gridCell = document.createElement("div");
-        
-        gridCell.classList.add("grid", 'grid-cell')
-
-        gridCell.addEventListener("mousedown",() => {
-            gridCell.style.background = "yellow"
-        })
-
-        paintingWall.appendChild(gridCell)
-    }
+function setGridToTheStart(){
+    clearGrid()
+    createGrid()
+    setGridSize()
 }
 
 
-const gridCells = document.querySelectorAll(".grid-cell");
-gridCells.forEach((cell) =>{
-    cell.style.width = `${(wallWidth / gridSize)}rem`;
-    cell.style.height = `${(wallHeight / gridSize)}rem`;
-});
+function createGrid(){
+    for(let cells = 0; cells < (gridSize * gridSize); cells++){
+            let gridCell = document.createElement("div");
+
+            gridCell.addEventListener("mouseover",changeClr);
+            gridCell.addEventListener("mousedown",changeClr);
+
+            paintingWall.appendChild(gridCell).classList.add("grid", 'grid-cell');
+        }
+}
+
+
+
+
+//*change Color Mode
+function changeClr(e){
+    if(e.type === "mouseover" && !mouseDown) return;
+    
+    if(currentMode === "rainbow"){
+        let clr1 = Math.floor(Math.random() * 256);
+        let clr2 = Math.floor(Math.random() * 256);
+        let clr3 = Math.floor(Math.random() * 256);
+    
+        e.target.style.background = `rgb(${clr1},${clr2},${clr3})`;
+    }else if(currentMode === "default"){
+        e.target.style.background = "black"
+    }else if(currentMode === "eraser"){
+        e.target.style.background = "white"
+    }
+    else if(currentMode === "penChanger"){
+        e.target.style.background = value
+    }
+
+}
+
+
+
+//*clears the grid Color
+function clearGrid(){
+    paintingWall.innerHTML = ""
+}
+
+//*sets the grid Size for each cell
+function setGridSize(){
+    wallWidthSize = paintingWall.style.width = "40rem";
+    wallHeightSize = paintingWall.style.height = "40rem";
+
+
+    let wallWidth = wallWidthSize.replace("rem","");
+    let wallHeight = wallHeightSize.replace("rem","");
+
+    const gridCells = document.querySelectorAll(".grid-cell");
+
+    gridCells.forEach((cell) =>{
+        cell.style.width = `${(wallWidth / gridSize)}rem`;
+        cell.style.height = `${(wallHeight / gridSize)}rem`;
+    });
+}
